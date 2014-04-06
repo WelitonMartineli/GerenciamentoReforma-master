988620463b1f8d9a87b397a00da45e29cdfec2dc
WelitonMartineli <welitonmartineli@gmail.com>
2014-04-06 13:23:00 -0300

    Modificacao de imagens

--- gen/br/com/mackenzie/R.java
+++ gen/br/com/mackenzie/R.java
@@ -11,41 +11,42 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_ambientes=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_servicos=0x7f020002;
-        public static final int tijolos=0x7f020003;
-        public static final int tijolos_principal=0x7f020004;
+        public static final int add_list=0x7f020000;
+        public static final int ic_ambientes=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_servicos=0x7f020003;
+        public static final int tijolos=0x7f020004;
+        public static final int tijolos_principal=0x7f020005;
     }
     public static final class id {
-        public static final int adicionar=0x7f07000b;
-        public static final int ambientes=0x7f070017;
-        public static final int btnDisplay=0x7f070013;
+        public static final int adicionar=0x7f07000d;
+        public static final int ambientes=0x7f070018;
+        public static final int btnDisplay=0x7f070015;
         public static final int button1=0x7f07001c;
         public static final int cancelar=0x7f07000a;
-        public static final int chkAcabamentoJanela=0x7f070011;
-        public static final int chkAcabamentoParede=0x7f070010;
-        public static final int chkAcabamentoPorta=0x7f070012;
-        public static final int chkGesso=0x7f07000e;
-        public static final int chkPintura=0x7f07000f;
+        public static final int chkAcabamentoJanela=0x7f070013;
+        public static final int chkAcabamentoParede=0x7f070012;
+        public static final int chkAcabamentoPorta=0x7f070014;
+        public static final int chkGesso=0x7f070010;
+        public static final int chkPintura=0x7f070011;
         public static final int exluir=0x7f070009;
-        public static final int imageView1=0x7f070015;
+        public static final int imageView1=0x7f070017;
         public static final int janela=0x7f070005;
         public static final int labelJanela=0x7f070004;
         public static final int labelMetragem=0x7f070006;
         public static final int labelNome=0x7f070000;
         public static final int labelPorta=0x7f070002;
-        public static final int listaAmbientes=0x7f07000c;
+        public static final int listaAmbientes=0x7f07000e;
         public static final int metragem=0x7f070007;
         public static final int nome=0x7f070001;
-        public static final int nomeAmbiente=0x7f07000d;
+        public static final int nomeAmbiente=0x7f07000f;
         public static final int porta=0x7f070003;
         public static final int salvar=0x7f070008;
-        public static final int servicos=0x7f070018;
-        public static final int tableRow1=0x7f070016;
-        public static final int tableRow2=0x7f070019;
-        public static final int tableRow3=0x7f070014;
-        public static final int textView1=0x7f07001a;
+        public static final int servicos=0x7f070019;
+        public static final int tableRow1=0x7f07000b;
+        public static final int tableRow2=0x7f07001a;
+        public static final int tableRow3=0x7f070016;
+        public static final int textView1=0x7f07000c;
         public static final int textView2=0x7f07001b;
     }
     public static final class layout {
